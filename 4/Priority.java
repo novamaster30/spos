d27790df7d5b109705f49f6a74befdb34f49a85a
novamaster30 <120588567+novamaster30@users.noreply.github.com>
2023-11-06 06:13:47 +0530

    Priority.java

--- 4/Priority.java
+++ 4/Priority.java
@@ -10,29 +10,29 @@ public class Priority {
 		int process[]=new int[n];
 		int priority[]=new int[n];
 		int arrival[]=new int[n];
-		int brust[]=new int[n];
+		int burst[]=new int[n];
 		int completion[]=new int[n];
 		int WT[]=new int[n];
 		int TAT[]=new int[n];
-		int sum=0;
-				float totalaroundtime=0,waittime=0;
+		float AvgTAT=0,AvgWT=0;
 		
 		for(int i=0;i<n;i++)
 		{
-			System.out.print("Enter Process "+(i+1)+"Brust Time :");
-			brust[i]=sn.nextInt();
+			System.out.print("Enter Process "+(i+1)+"burst Time :");
+			burst[i]=sn.nextInt();
 			
 			System.out.print("Enter Process "+(i+1)+"Priority  :");
 			priority[i]=sn.nextInt();
 			
 			process[i]=i+1;
+			arrival[i]=0;
 		}
 		
 		for(int i=0;i<n-1;i++)
 		{
 			for(int j=i+1;j<n;j++)
 			{
-				if(priority[i]<priority[j])
+				if(priority[i]>priority[j])
 				{
 					int temp=priority[i];
 					priority[i]=priority[j];
@@ -42,35 +42,36 @@ public class Priority {
 					process[i]=process[j];
 					process[j]=temp;
 					
-					temp=brust[i];
-					brust[i]=brust[j];
-					brust[j]=temp;
+					temp=burst[i];
+					burst[i]=burst[j];
+					burst[j]=temp;
+
 					
 				}
 			}
 			
 		}
-			for(int i=0;i<n;i++)
-			{
-				sum=sum+brust[i];
-				completion[i]=sum;
-			}
 			
-			System.out.print("\n\nPriority \tProcess \tBrusttime \tCompletion \tWT \tTAT");
+        completion[0] = arrival[0] + burst[0];
+        for (int i = 1; i < n; i++) {
+            completion[i] = Math.max(arrival[i], completion[i - 1]) + burst[i];
+        }
+
+			
+			System.out.print("\n\nPriority \tProcess \tbursttime \tCompletion \tWT \t\tTAT");
 			for(int i=0;i<n;i++)
 			{
 				TAT[i]=completion[i]-arrival[i];
-				totalaroundtime=totalaroundtime+TAT[i];
+				AvgTAT+=TAT[i];
 				
-				WT[i]=TAT[i]-brust[i];
-				waittime=waittime+WT[i];
+				WT[i]=TAT[i]-burst[i];
+				AvgWT+=WT[i];
 				
-				System.out.print("\n"+priority[i]+"\t\t"+process[i]+"\t\t"+brust[i]+"\t\t"+completion[i]+"\t\t"+WT[i]+"\t\t"+TAT[i]);
+				System.out.print("\n"+priority[i]+"\t\t"+process[i]+"\t\t"+burst[i]+"\t\t"+completion[i]+"\t\t"+WT[i]+"\t\t"+TAT[i]);
 			}
-			System.out.print("\n\nAvergae Turn Around Time :"+(totalaroundtime/n));
-			System.out.print("\nAvergae Wait Time :"+(waittime/n));
+			System.out.print("\n\nAvergae Turn Around Time :"+(AvgTAT/n));
+			System.out.print("\nAvergae Wait Time :"+(AvgWT/n));
 			
 			
 		}
 	}
-
